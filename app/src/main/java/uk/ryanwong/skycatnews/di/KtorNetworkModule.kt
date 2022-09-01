aeedafd8c7a14f2a5a782eb43faea5637ae8da25
Ryan Wong <ryan@rwmobi.com>
2022-09-01 19:44:05 +0100

    [refactor] DI: rearrange DI modules

---
+++ app/src/main/java/uk/ryanwong/skycatnews/di/KtorNetworkModule.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2022. Ryan Wong (hello@ryanwong.co.uk)
+ */
+
+package uk.ryanwong.skycatnews.uk.ryanwong.skycatnews.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import javax.inject.Singleton
+import kotlinx.serialization.json.Json
+
+@Module
+@InstallIn(SingletonComponent::class)
+object KtorNetworkModule {
+    @Provides
+    @Singleton
+    fun provideHttpClient(): HttpClient {
+        return HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(
+                    Json {
+                        ignoreUnknownKeys = true
+                        prettyPrint = true
+                        isLenient = true
+                    }
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/uk/ryanwong/skycatnews/storydetail/data/di/DataModule.kt
+++ app/src/main/java/uk/ryanwong/skycatnews/storydetail/data/di/DataModule.kt
@@ -2,22 +2,22 @@
  * Copyright (c) 2022. Ryan Wong (hello@ryanwong.co.uk)
  */
 
-package uk.ryanwong.skycatnews.uk.ryanwong.skycatnews.newslist.data.di
+package uk.ryanwong.skycatnews.uk.ryanwong.skycatnews.storydetail.data.di
 
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
-import uk.ryanwong.skycatnews.uk.ryanwong.skycatnews.newslist.data.remote.FakeNewsListService
-import uk.ryanwong.skycatnews.uk.ryanwong.skycatnews.newslist.data.remote.NewsListService
+import uk.ryanwong.skycatnews.uk.ryanwong.skycatnews.storydetail.data.remote.FakeStoryService
+import uk.ryanwong.skycatnews.uk.ryanwong.skycatnews.storydetail.data.remote.StoryService
 
 @Module
 @InstallIn(ViewModelComponent::class)
 object DataModule {
     @Provides
     @ViewModelScoped
-    fun provideNewsListService(): NewsListService {
-        return FakeNewsListService()
+    fun provideStoryService(): StoryService {
+        return FakeStoryService()
     }
 }
