9a4b207cb41342d1b973d24c57c05a38a070353a
Ryan Wong <ryan@rwmobi.com>
2022-09-02 21:06:06 +0100

    [chore] KtorNetworkModule: apply code formatting

--- app/src/main/java/uk/ryanwong/skycatnews/di/KtorNetworkModule.kt
+++ app/src/main/java/uk/ryanwong/skycatnews/di/KtorNetworkModule.kt
@@ -12,8 +12,8 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
-import javax.inject.Singleton
 import kotlinx.serialization.json.Json
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
