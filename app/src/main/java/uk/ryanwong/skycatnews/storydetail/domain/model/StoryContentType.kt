2bac4bc45deee8886243d9a7de097c59b43c017a
Ryan Wong <ryan@rwmobi.com>
2022-09-04 20:38:52 +0100

    [feature] StoryContentType: implement StoryContentType.parse()

--- app/src/main/java/uk/ryanwong/skycatnews/storydetail/domain/model/StoryContentType.kt
+++ app/src/main/java/uk/ryanwong/skycatnews/storydetail/domain/model/StoryContentType.kt
@@ -7,5 +7,17 @@ package uk.ryanwong.skycatnews.storydetail.domain.model
 enum class StoryContentType {
     UNKNOWN,
     IMAGE,
-    PARAGRAPH,
+    PARAGRAPH;
+
+    companion object {
+        fun parse(storyContentType: String?): StoryContentType {
+            return storyContentType?.let {
+                try {
+                    StoryContentType.valueOf(it)
+                } catch (e: IllegalArgumentException) {
+                    StoryContentType.UNKNOWN
+                }
+            } ?: StoryContentType.UNKNOWN
+        }
+    }
 }
