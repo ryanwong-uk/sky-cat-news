27d56dd4fb77403807822d9eee32339a11c044ab
Ryan Wong <ryan@rwmobi.com>
2022-09-04 16:57:54 +0100

    [test] News List: add DAO unit tests for news list

--- app/src/androidTest/java/uk/ryanwong/skycatnews/newslist/data/local/NewsListDaoTest.kt
+++ app/src/androidTest/java/uk/ryanwong/skycatnews/newslist/data/local/NewsListDaoTest.kt
@@ -9,6 +9,7 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
@@ -17,17 +18,18 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import uk.ryanwong.skycatnews.app.database.LocalDatabaseImpl
+import uk.ryanwong.skycatnews.newslist.data.local.model.NewsItemEntity
 import uk.ryanwong.skycatnews.newslist.data.local.model.NewsListEntity
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 @MediumTest
-class NewsListDaoTest {
+internal class NewsListDaoTest {
     private lateinit var localDatabase: LocalDatabaseImpl
     private lateinit var newsListDao: NewsListDao
 
     /**
-     * Test plan: basic CRUD usages
+     * Test plan: basic CRUD usages with some special cases
      */
 
     @Before
@@ -106,5 +108,223 @@ class NewsListDaoTest {
         // Then
         val returnedTitle = newsListDao.getNewsListTitle(listId = listId)
         returnedTitle shouldBe null
+    }
+
+    /***
+     * News List (in the form of List<NewsItemEntity>)
+     */
+    @Test
+    fun emptyTable_InsertEmptyNewsList_ReturnEmptyList() = runTest {
+        // Given
+        val listId = 1
+        val newsItems = emptyList<NewsItemEntity>()
+        newsListDao.getNewsList(listId = listId) shouldBe emptyList()
+
+        // When
+        newsListDao.insertNewsItems(newsItems = newsItems)
+
+        // Then
+        val returnedNewsList = newsListDao.getNewsList(listId = listId)
+        returnedNewsList shouldBe emptyList()
+    }
+
+    @Test
+    fun emptyTable_InsertOneNewsItem_ReturnOneNewsItem() = runTest {
+        // Given
+        val listId = 1
+        val newsItems = listOf(
+            NewsItemEntity(
+                listId = listId,
+                newsId = 1,
+                type = "some-type",
+                headline = "some-headline",
+                creationDate = "2022-5-21T00:00:00Z",
+                modifiedDate = "2022-5-21T00:00:00Z",
+                url = "https://some.url/",
+                weblinkUrl = "https://some.weblink.url/",
+                teaserText = "some-teaser-text",
+                teaserImageHref = "https://some.teaser.image/href",
+                teaserImageTemplated = true,
+                teaserImageType = "some-teaser-image-type",
+                teaserImageAccessibilityText = "some-teaser-image-accessibility-text"
+            )
+        )
+        newsListDao.getNewsList(listId = listId) shouldBe emptyList()
+
+        // When
+        newsListDao.insertNewsItems(newsItems = newsItems)
+
+        // Then
+        val returnedNewsList = newsListDao.getNewsList(listId = listId)
+        returnedNewsList shouldBe newsItems
+    }
+
+    @Test
+    fun emptyTable_InsertMultipleNewsItems_ReturnMultipleNewsItems() = runTest {
+        // Given
+        val listId = 1
+        val newsItems = listOf(
+            NewsItemEntity(
+                listId = listId,
+                newsId = 1,
+                type = "some-type",
+                headline = "some-headline",
+                creationDate = "2022-5-21T00:00:00Z",
+                modifiedDate = "2022-5-21T00:00:00Z",
+                url = "https://some.url/",
+                weblinkUrl = "https://some.weblink.url/",
+                teaserText = "some-teaser-text",
+                teaserImageHref = "https://some.teaser.image/href",
+                teaserImageTemplated = true,
+                teaserImageType = "some-teaser-image-type",
+                teaserImageAccessibilityText = "some-teaser-image-accessibility-text"
+            ),
+            NewsItemEntity(
+                listId = listId,
+                newsId = 2,
+                type = "some-type-2",
+                headline = "some-headline-2",
+                creationDate = "2022-5-21T00:00:01Z",
+                modifiedDate = "2022-5-21T00:00:01Z",
+                url = "https://some.url/2",
+                weblinkUrl = "https://some.weblink.url/2",
+                teaserText = "some-teaser-text-2",
+                teaserImageHref = "https://some.teaser.image/href/2",
+                teaserImageTemplated = true,
+                teaserImageType = "some-teaser-image-type-2",
+                teaserImageAccessibilityText = "some-teaser-image-accessibility-text-2"
+            ),
+            NewsItemEntity(
+                listId = listId,
+                newsId = 3,
+                type = "some-type-3",
+                headline = "some-headline-3",
+                creationDate = "2022-5-21T00:00:02Z",
+                modifiedDate = "2022-5-21T00:00:02Z",
+                url = "https://some.url/3",
+                weblinkUrl = "https://some.weblink.url/3",
+                teaserText = "some-teaser-text-3",
+                teaserImageHref = "https://some.teaser.image/href/3",
+                teaserImageTemplated = true,
+                teaserImageType = "some-teaser-image-type-3",
+                teaserImageAccessibilityText = "some-teaser-image-accessibility-text-3"
+            )
+        )
+        newsListDao.getNewsList(listId = listId) shouldBe emptyList()
+
+        // When
+        newsListDao.insertNewsItems(newsItems = newsItems)
+
+        // Then
+        val returnedNewsList = newsListDao.getNewsList(listId = listId)
+        returnedNewsList shouldContainExactlyInAnyOrder newsItems
+    }
+
+    @Test
+    fun existingNewsList_ReplaceNewsItem_ReturnUpdatedNewsList() = runTest {
+        // Given
+        val listId = 1
+        val newsItemEntityOne = NewsItemEntity(
+            listId = listId,
+            newsId = 1,
+            type = "some-type",
+            headline = "some-headline",
+            creationDate = "2022-5-21T00:00:00Z",
+            modifiedDate = "2022-5-21T00:00:00Z",
+            url = "https://some.url/",
+            weblinkUrl = "https://some.weblink.url/",
+            teaserText = "some-teaser-text",
+            teaserImageHref = "https://some.teaser.image/href",
+            teaserImageTemplated = true,
+            teaserImageType = "some-teaser-image-type",
+            teaserImageAccessibilityText = "some-teaser-image-accessibility-text"
+        )
+        val newsItemEntityTwo = NewsItemEntity(
+            listId = listId,
+            newsId = 2,
+            type = "some-type-2",
+            headline = "some-headline-2",
+            creationDate = "2022-5-21T00:00:01Z",
+            modifiedDate = "2022-5-21T00:00:01Z",
+            url = "https://some.url/2",
+            weblinkUrl = "https://some.weblink.url/2",
+            teaserText = "some-teaser-text-2",
+            teaserImageHref = "https://some.teaser.image/href/2",
+            teaserImageTemplated = true,
+            teaserImageType = "some-teaser-image-type-2",
+            teaserImageAccessibilityText = "some-teaser-image-accessibility-text-2"
+        )
+        val newsItemEntityOneModified = NewsItemEntity(
+            listId = listId,
+            newsId = 1,
+            type = "some-type-3",
+            headline = "some-headline-3",
+            creationDate = "2022-5-21T00:00:02Z",
+            modifiedDate = "2022-5-21T00:00:02Z",
+            url = "https://some.url/3",
+            weblinkUrl = "https://some.weblink.url/3",
+            teaserText = "some-teaser-text-3",
+            teaserImageHref = "https://some.teaser.image/href/3",
+            teaserImageTemplated = true,
+            teaserImageType = "some-teaser-image-type-3",
+            teaserImageAccessibilityText = "some-teaser-image-accessibility-text-3"
+        )
+        val newsItems = listOf(newsItemEntityOne, newsItemEntityTwo)
+        newsListDao.insertNewsItems(newsItems = newsItems)
+
+        // When
+        newsListDao.insertNewsItems(newsItems = listOf(newsItemEntityOneModified))
+
+        // Then
+        val returnedNewsList = newsListDao.getNewsList(listId = listId)
+        returnedNewsList shouldContainExactlyInAnyOrder listOf(
+            newsItemEntityOneModified,
+            newsItemEntityTwo
+        )
+    }
+
+    @Test
+    fun existingNewsList_DeleteNewsList_ReturnEmptyList() = runTest {
+        // Given
+        val listId = 1
+        val newsItemEntityOne = NewsItemEntity(
+            listId = listId,
+            newsId = 1,
+            type = "some-type",
+            headline = "some-headline",
+            creationDate = "2022-5-21T00:00:00Z",
+            modifiedDate = "2022-5-21T00:00:00Z",
+            url = "https://some.url/",
+            weblinkUrl = "https://some.weblink.url/",
+            teaserText = "some-teaser-text",
+            teaserImageHref = "https://some.teaser.image/href",
+            teaserImageTemplated = true,
+            teaserImageType = "some-teaser-image-type",
+            teaserImageAccessibilityText = "some-teaser-image-accessibility-text"
+        )
+        val newsItemEntityTwo = NewsItemEntity(
+            listId = listId,
+            newsId = 2,
+            type = "some-type-2",
+            headline = "some-headline-2",
+            creationDate = "2022-5-21T00:00:01Z",
+            modifiedDate = "2022-5-21T00:00:01Z",
+            url = "https://some.url/2",
+            weblinkUrl = "https://some.weblink.url/2",
+            teaserText = "some-teaser-text-2",
+            teaserImageHref = "https://some.teaser.image/href/2",
+            teaserImageTemplated = true,
+            teaserImageType = "some-teaser-image-type-2",
+            teaserImageAccessibilityText = "some-teaser-image-accessibility-text-2"
+        )
+        val newsItems = listOf(newsItemEntityOne, newsItemEntityTwo)
+        newsListDao.insertNewsItems(newsItems = newsItems)
+
+        // When
+        newsListDao.deleteNewsItems(listId = listId)
+
+        // Then
+        val returnedNewsList = newsListDao.getNewsList(listId = listId)
+        returnedNewsList shouldBe emptyList()
     }
 }
