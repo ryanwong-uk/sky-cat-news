3c6657cb369785870e8c7fe123d11cdccd3138ee
Ryan Wong <ryan@rwmobi.com>
2022-09-04 23:25:49 +0100

    [feature] Story Detail: implement Entity fromDto() functions

--- app/src/main/java/uk/ryanwong/skycatnews/storydetail/data/local/entity/ContentEntity.kt
+++ app/src/main/java/uk/ryanwong/skycatnews/storydetail/data/local/entity/ContentEntity.kt
@@ -7,17 +7,34 @@ package uk.ryanwong.skycatnews.storydetail.data.local.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import uk.ryanwong.skycatnews.storydetail.data.remote.model.ContentDto
 
 @Entity(tableName = "contents")
 data class ContentEntity(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "sequence_id")
-    val sequenceId: Int,
+    val sequenceId: Int = 0,
     @ColumnInfo(name = "story_id")
     val storyId: Int,
-    val type: String,
-    val url: String,
+    val type: String?,
+    val url: String?,
     @ColumnInfo(name = "accessibility_text")
-    val accessibilityText: String,
-    val text: String,
-)
+    val accessibilityText: String?,
+    val text: String?,
+) {
+    companion object {
+        fun fromDto(storyId: Int, contentDtoList: List<ContentDto>?): List<ContentEntity>? {
+            return contentDtoList?.map { content ->
+                with(content) {
+                    ContentEntity(
+                        storyId = storyId,
+                        type = type,
+                        url = url,
+                        accessibilityText = accessibilityText,
+                        text = text
+                    )
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/uk/ryanwong/skycatnews/storydetail/data/local/entity/StoryEntity.kt
+++ app/src/main/java/uk/ryanwong/skycatnews/storydetail/data/local/entity/StoryEntity.kt
@@ -7,19 +7,35 @@ package uk.ryanwong.skycatnews.storydetail.data.local.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import uk.ryanwong.skycatnews.storydetail.data.remote.model.StoryDto
 
 @Entity(tableName = "stories")
 data class StoryEntity(
     @PrimaryKey
     @ColumnInfo(name = "story_id")
     val storyId: Int,
-    val headline: String,
+    val headline: String?,
     @ColumnInfo(name = "hero_image_url")
-    val heroImageUrl: String,
+    val heroImageUrl: String?,
     @ColumnInfo(name = "hero_image_accessibility_text")
-    val heroImageAccessibilityText: String,
+    val heroImageAccessibilityText: String?,
     @ColumnInfo(name = "creation_date")
     val creationDate: String,
     @ColumnInfo(name = "modified_date")
     val modifiedDate: String,
-)
+) {
+    companion object {
+        fun fromDto(storyDto: StoryDto): StoryEntity {
+            with(storyDto) {
+                return StoryEntity(
+                    storyId = id,
+                    headline = headline,
+                    heroImageUrl = heroImage?.imageUrl,
+                    heroImageAccessibilityText = heroImage?.accessibilityText,
+                    creationDate = creationDate,
+                    modifiedDate = modifiedDate
+                )
+            }
+        }
+    }
+}
