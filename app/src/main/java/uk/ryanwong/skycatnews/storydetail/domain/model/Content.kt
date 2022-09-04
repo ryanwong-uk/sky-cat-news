7203d101146ffa1bfcc90713af2d23a7aadad006
Ryan Wong <ryan@rwmobi.com>
2022-09-04 22:38:12 +0100

    [feature] Story Detail: implement domain mails .fromEntity() function

--- app/src/main/java/uk/ryanwong/skycatnews/storydetail/domain/model/Content.kt
+++ app/src/main/java/uk/ryanwong/skycatnews/storydetail/domain/model/Content.kt
@@ -4,9 +4,26 @@
 
 package uk.ryanwong.skycatnews.storydetail.domain.model
 
+import uk.ryanwong.skycatnews.storydetail.data.local.model.ContentEntity
+
 data class Content(
     val accessibilityText: String?,
     val text: String,
     val type: StoryContentType,
     val url: String?,
-)
+) {
+    companion object {
+        fun fromEntity(
+            contentEntities: List<ContentEntity>,
+        ): List<Content> {
+            return contentEntities.map { contentEntity ->
+                Content(
+                    accessibilityText = contentEntity.accessibilityText,
+                    text = contentEntity.text,
+                    type = StoryContentType.parse(contentEntity.type),
+                    url = contentEntity.url
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/uk/ryanwong/skycatnews/storydetail/domain/model/Story.kt
+++ app/src/main/java/uk/ryanwong/skycatnews/storydetail/domain/model/Story.kt
@@ -4,6 +4,9 @@
 
 package uk.ryanwong.skycatnews.storydetail.domain.model
 
+import uk.ryanwong.skycatnews.storydetail.data.local.model.ContentEntity
+import uk.ryanwong.skycatnews.storydetail.data.local.model.StoryEntity
+
 data class Story(
     val id: Int,
     val contents: List<Content>,
@@ -11,4 +14,22 @@ data class Story(
     val headline: String,
     val heroImageAccessibilityText: String?,
     val heroImageUrl: String,
-)
+) {
+    companion object {
+        fun fromEntity(
+            storyEntity: StoryEntity?,
+            contentEntities: List<ContentEntity>,
+        ): Story? {
+            return storyEntity?.let { story ->
+                Story(
+                    id = story.storyId,
+                    contents = Content.fromEntity(contentEntities = contentEntities),
+                    date = story.modifiedDate,
+                    headline = story.headline,
+                    heroImageAccessibilityText = story.heroImageAccessibilityText,
+                    heroImageUrl = story.heroImageUrl
+                )
+            }
+        }
+    }
+}
