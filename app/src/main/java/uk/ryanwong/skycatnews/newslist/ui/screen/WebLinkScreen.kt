61c23c79007b4d9a7b632cb1c3a5333e4689f0b8
Ryan Wong <ryan@rwmobi.com>
2022-09-06 00:32:40 +0100

    [feature] WebLinkScreen: apply new webview

--- app/src/main/java/uk/ryanwong/skycatnews/newslist/ui/screen/WebLinkScreen.kt
+++ app/src/main/java/uk/ryanwong/skycatnews/newslist/ui/screen/WebLinkScreen.kt
@@ -4,32 +4,41 @@
 
 package uk.ryanwong.skycatnews.newslist.ui.screen
 
-import android.webkit.WebView
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.web.WebView
+import com.google.accompanist.web.rememberWebViewState
 
 @Composable
 fun WebLinkScreen(
     storyId: Int,
     modifier: Modifier = Modifier,
 ) {
-    val url = "https://www.rwmobi.com/"
-    AndroidView(
-        factory = { context ->
-            WebView(context).apply {
-                with(settings) {
-                    javaScriptEnabled = true
-                    domStorageEnabled = true
-                }
-                loadUrl(url)
-            }
-        },
-        update = { webView -> webView.loadUrl(url) },
-        modifier = modifier.fillMaxSize()
-    )
+    val webViewState = rememberWebViewState("https://www.rwmobi.com/")
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text("text = header",
+                modifier = Modifier.height(48.dp))
+        }
+        WebView(
+            state = webViewState,
+            onCreated = { it.settings.javaScriptEnabled = true },
+            modifier = Modifier.fillMaxSize(),
+        )
+    }
 }
 
 @Preview
