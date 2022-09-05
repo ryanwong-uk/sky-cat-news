fa197d2f347001b21c18d31c32003c3f5f5644d1
Ryan Wong <ryan@rwmobi.com>
2022-09-05 16:55:58 +0100

    [test] NewsItemEntityTest: add unit tests

---
+++ app/src/test/java/uk/ryanwong/skycatnews/newslist/data/local/entity/NewsItemEntityTest.kt
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2022. Ryan Wong (hello@ryanwong.co.uk)
+ */
+
+package uk.ryanwong.skycatnews.newslist.data.local.entity
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.shouldBe
+import uk.ryanwong.skycatnews.newslist.data.remote.model.NewsItemDto
+
+class NewsItemEntityTest : FreeSpec() {
+
+    init {
+        "fromDto" - {
+            "Should skip processing NewsItemDto in the list if id is null" {
+                // Given
+                val newsItemDtoList = listOf(
+                    NewsItemEntityTestData.mockNewsItemDto.copy(
+                        id = null
+                    )
+                )
+
+                // When
+                val newsItemEntity =
+                    NewsItemEntity.fromDto(listId = 1, newsItemDtoList = newsItemDtoList)
+
+                // Then
+                newsItemEntity shouldBe null
+            }
+
+            "Should skip processing NewsItemDto in the list if creationDate is null" {
+                // Given
+                val newsItemDtoList = listOf(
+                    NewsItemEntityTestData.mockNewsItemDto.copy(
+                        creationDate = null
+                    )
+                )
+
+                // When
+                val newsItemEntity =
+                    NewsItemEntity.fromDto(listId = 1, newsItemDtoList = newsItemDtoList)
+
+                // Then
+                newsItemEntity shouldBe null
+            }
+
+            "Should skip processing NewsItemDto in the list if modifiedDate is null" {
+                // Given
+                val newsItemDtoList = listOf(
+                    NewsItemEntityTestData.mockNewsItemDto.copy(
+                        modifiedDate = null
+                    )
+                )
+
+                // When
+                val newsItemEntity =
+                    NewsItemEntity.fromDto(listId = 1, newsItemDtoList = newsItemDtoList)
+
+                // Then
+                newsItemEntity shouldBe null
+            }
+
+            "Should return null if newsItemDtoList is null" {
+                // Given
+                val newsItemDtoList = null
+
+                // When
+                val newsItemEntity =
+                    NewsItemEntity.fromDto(listId = 1, newsItemDtoList = newsItemDtoList)
+
+                // Then
+                newsItemEntity shouldBe null
+            }
+
+            "Should return empty list if newsItemDtoList is empty" {
+                // Given
+                val newsItemDtoList = emptyList<NewsItemDto>()
+
+                // When
+                val newsItemEntity =
+                    NewsItemEntity.fromDto(listId = 1, newsItemDtoList = newsItemDtoList)
+
+                // Then
+                newsItemEntity shouldBe emptyList()
+            }
+
+            "Should correctly convert a newsItemDtoList with one item" {
+                // Given
+                val newsItemDtoList = listOf(NewsItemEntityTestData.mockNewsItemDto)
+
+                // When
+                val newsItemEntity =
+                    NewsItemEntity.fromDto(listId = 1, newsItemDtoList = newsItemDtoList)
+
+                // Then
+                newsItemEntity shouldBe listOf(NewsItemEntityTestData.mockNewsItemEntity)
+            }
+
+            "Should correctly convert a newsItemDtoList with multiple items" {
+                // Given
+                val newsItemDtoList = listOf(NewsItemEntityTestData.mockNewsItemDto,
+                    NewsItemEntityTestData.mockNewsItemDto2,
+                    NewsItemEntityTestData.mockNewsItemDto3)
+
+                // When
+                val newsItemEntity =
+                    NewsItemEntity.fromDto(listId = 1, newsItemDtoList = newsItemDtoList)
+
+                // Then
+                newsItemEntity shouldContainExactlyInAnyOrder listOf(NewsItemEntityTestData.mockNewsItemEntity,
+                    NewsItemEntityTestData.mockNewsItemEntity2,
+                    NewsItemEntityTestData.mockNewsItemEntity3)
+            }
+        }
+    }
+}
---
+++ app/src/test/java/uk/ryanwong/skycatnews/newslist/data/local/entity/NewsItemEntityTestData.kt
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2022. Ryan Wong (hello@ryanwong.co.uk)
+ */
+
+package uk.ryanwong.skycatnews.newslist.data.local.entity
+
+import uk.ryanwong.skycatnews.newslist.data.remote.model.LinksDto
+import uk.ryanwong.skycatnews.newslist.data.remote.model.NewsItemDto
+import uk.ryanwong.skycatnews.newslist.data.remote.model.TeaserImageDto
+import uk.ryanwong.skycatnews.newslist.data.remote.model.UrlDto
+
+object NewsItemEntityTestData {
+
+    val mockNewsItemDto = NewsItemDto(
+        id = 1,
+        creationDate = "2020-11-18T00:00:00Z",
+        headline = "some-headline",
+        modifiedDate = "2020-11-19T00:00:00Z",
+        teaserImage = TeaserImageDto(
+            links = LinksDto(
+                url = UrlDto(
+                    href = "https://some.links.url/href",
+                    templated = true,
+                    type = "image/jpeg"
+                )
+            ),
+            accessibilityText = "some-accessibility-text"
+        ),
+        teaserText = "some-teaser-text",
+        type = "story",
+        url = "https://some.url/",
+        weblinkUrl = "https://some.weblink.url/"
+    )
+
+    val mockNewsItemEntity = NewsItemEntity(listId = 1,
+        newsId = 1,
+        type = "story",
+        headline = "some-headline",
+        creationDate = "2020-11-18T00:00:00Z",
+        modifiedDate = "2020-11-19T00:00:00Z",
+        url = "https://some.url/",
+        weblinkUrl = "https://some.weblink.url/",
+        teaserText = "some-teaser-text",
+        teaserImageHref = "https://some.links.url/href",
+        teaserImageTemplated = true,
+        teaserImageType = "image/jpeg",
+        teaserImageAccessibilityText = "some-accessibility-text")
+
+    val mockNewsItemDto2 = NewsItemDto(
+        id = 2,
+        creationDate = "2020-11-18T00:00:00Z",
+        headline = "some-headline",
+        modifiedDate = "2020-11-19T00:00:00Z",
+        teaserImage = TeaserImageDto(
+            links = LinksDto(
+                url = UrlDto(
+                    href = "https://some.links.url/href",
+                    templated = true,
+                    type = "image/jpeg"
+                )
+            ),
+            accessibilityText = "some-accessibility-text"
+        ),
+        teaserText = "some-teaser-text-2",
+        type = "story",
+        url = "https://some.url/",
+        weblinkUrl = "https://some.weblink.url/"
+    )
+
+    val mockNewsItemEntity2 = NewsItemEntity(listId = 1,
+        newsId = 2,
+        type = "story",
+        headline = "some-headline",
+        creationDate = "2020-11-18T00:00:00Z",
+        modifiedDate = "2020-11-19T00:00:00Z",
+        url = "https://some.url/",
+        weblinkUrl = "https://some.weblink.url/",
+        teaserText = "some-teaser-text-2",
+        teaserImageHref = "https://some.links.url/href",
+        teaserImageTemplated = true,
+        teaserImageType = "image/jpeg",
+        teaserImageAccessibilityText = "some-accessibility-text")
+
+    val mockNewsItemDto3 = NewsItemDto(
+        id = 3,
+        creationDate = "2020-11-18T00:00:00Z",
+        headline = "some-headline",
+        modifiedDate = "2020-11-19T00:00:00Z",
+        teaserImage = TeaserImageDto(
+            links = LinksDto(
+                url = UrlDto(
+                    href = "https://some.links.url/href",
+                    templated = true,
+                    type = "image/jpeg"
+                )
+            ),
+            accessibilityText = "some-accessibility-text"
+        ),
+        teaserText = "some-teaser-text-3",
+        type = "story",
+        url = "https://some.url/",
+        weblinkUrl = "https://some.weblink.url/"
+    )
+
+    val mockNewsItemEntity3 = NewsItemEntity(listId = 1,
+        newsId = 3,
+        type = "story",
+        headline = "some-headline",
+        creationDate = "2020-11-18T00:00:00Z",
+        modifiedDate = "2020-11-19T00:00:00Z",
+        url = "https://some.url/",
+        weblinkUrl = "https://some.weblink.url/",
+        teaserText = "some-teaser-text-3",
+        teaserImageHref = "https://some.links.url/href",
+        teaserImageTemplated = true,
+        teaserImageType = "image/jpeg",
+        teaserImageAccessibilityText = "some-accessibility-text")
+}
