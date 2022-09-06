ca24737c7741369fecc64c724e58550a1c3dd9cc
Ryan Wong <ryan@rwmobi.com>
2022-09-06 17:14:59 +0100

    [chore] FakeNewsListService: changed sample teaser text

--- app/src/main/java/uk/ryanwong/skycatnews/newslist/data/remote/FakeNewsListService.kt
+++ app/src/main/java/uk/ryanwong/skycatnews/newslist/data/remote/FakeNewsListService.kt
@@ -30,7 +30,7 @@ class FakeNewsListService : NewsListService {
                             ),
                             accessibilityText = "Image content description"
                         ),
-                        teaserText = "Story teaser text",
+                        teaserText = "Signs behold brought over give the also. Fish hath void. Face. Sixth appear all own spirit. Set can't fowl had fowl fish fowl male living form life winged two form from fifth he evening fowl abundantly gathered own wherein blessed. Forth fruit kind the is them herb divided moveth land, deep abundantly good gathering after. Earth good him day rule fish fill place which created his she'd fill, is Together itself had second Fourth lesser be us beginning earth. Dry meat don't winged under seas of own hath signs fruitful, evening. You'll of. Lesser heaven lights have tree light, us.",
                         type = "story",
                         advertUrl = null,
                         weblinkUrl = null
@@ -50,7 +50,7 @@ class FakeNewsListService : NewsListService {
                             ),
                             accessibilityText = "Image content description"
                         ),
-                        teaserText = "Story teaser text",
+                        teaserText = "Created said male greater form. Likeness the light grass they're darkness saw thing set set doesn't fruit without after was creepeth abundantly is is a unto us so very thing let beginning living gathering seasons thing very, darkness brought you'll fruit earth signs creature air light moving bring seasons and saw. Second living green years, him it every fruitful saying one also creature for waters saw morning fifth. Of dry seas creepeth, unto place creature days. Female void called gathered herb him grass can't tree set. They're yielding earth. The wherein air dominion god blessed made us open seed don't.",
                         type = "story",
                         advertUrl = null,
                         weblinkUrl = null
@@ -101,7 +101,7 @@ class FakeNewsListService : NewsListService {
                             ),
                             accessibilityText = "Image content description"
                         ),
-                        teaserText = "Story teaser text",
+                        teaserText = "Created shall divided winged above spirit. Green, waters first seed evening saw for hath male make moving every set forth cattle herb behold i them is. Yielding, you'll great whales us winged own whose over to created green darkness sixth also fly itself won't you're won't. Moved she'd greater fruit fruitful whales called bring seasons lesser in itself living earth be own heaven moved fowl from us form. Fill appear cattle she'd open. Fill two saying hath Have open you from in light dry cattle man very waters over to whose herb was sea great given god darkness that first.",
                         type = "story",
                         advertUrl = null,
                         weblinkUrl = null
