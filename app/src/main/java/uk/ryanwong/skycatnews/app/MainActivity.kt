91f7b743c730d55ad4998985039637b0b0e5d31e
Ryan Wong <ryan@rwmobi.com>
2022-09-07 07:55:37 +0100

    [chore] MainActivity: remove broken preview

--- app/src/main/java/uk/ryanwong/skycatnews/app/MainActivity.kt
+++ app/src/main/java/uk/ryanwong/skycatnews/app/MainActivity.kt
@@ -8,7 +8,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -63,13 +62,5 @@ private fun SkyCatNewsApp() {
         ) {
             WebLinkScreen()
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SkyCatNewsTheme {
-        SkyCatNewsApp()
     }
 }
